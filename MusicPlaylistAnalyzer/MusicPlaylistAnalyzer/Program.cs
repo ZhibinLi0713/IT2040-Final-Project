142e014a2358e1bc19ba9e2f751c9686031094e8
Zhibin Li <42685196+ZhibinLi0713@users.noreply.github.com>
2018-12-06 15:26:19 -0600

    1

--- MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Program.cs
+++ MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Program.cs
@@ -1,81 +1,171 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MusicPlaylistAnalyzer
 {
     class Program
     {
-         static void Main(string[] args)
+
+        static List<Song> ReadSongs(string filepath)
         {
-            Console.WriteLine($"the arguments are {args[0]} and {args[1]}");
-            if (args.Length == 2)
+            List<Song> result = new List<Song>();
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            var line = sr.ReadLine();
+            line = sr.ReadLine();
+            while (line != null && line.Trim()!="")
             {
-                List<Song> songList = new List<Song>();
-                if (Utilities.ReadFromFile(args[0], songList))
+                var parts = line.Split('\t');
+                if (parts.Length == 8)
                 {
-                    String result = GenerateReport(songList);
-                    if(Utilities.WriteToFile(args[1], result))
+                    try
                     {
-                        Console.WriteLine("Report created successfull");
+                        result.Add(new Song
+                        {
+                            Name = parts[0],
+                            Artist = parts[1],
+                            Album = parts[2],
+                            Genre = parts[3],
+                            Size = Convert.ToInt32(parts[4]),
+                            Time = Convert.ToInt32(parts[5]),
+                            Year = Convert.ToInt32(parts[6]),
+                            Plays = Convert.ToInt32(parts[7])
+                        });
                     }
-
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Record contains data that is not of the right type.");
+                        throw new Exception();
+                    }
+                }else
+                {
+                    Console.WriteLine("Record doesn't contain the correct number of data elements.");
+                    throw new Exception();
                 }
+                line = sr.ReadLine();
             }
-            else
+            fs.Close();
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length==0)
             {
-                Console.WriteLine("Unable to run without 2 arguments, Correct Systax:- MusicPlaylistAnalyzer <music_playlist_file_path> <report_file_path>");
+                Console.WriteLine("Please enter 2 command line parameters!");
+                return;
             }
+            string infile = args[0];
+            string outfile = args[1];
 
-
-        }
-
-       static String GenerateReport(List<Song> songs)
-        {
-            String result = "";
-            result += "Music Playlist Report\n\n";
-            var moreThen200Plays = from song in songs where song.Plays >= 200 select song;
-            result = AddSongsToResult(result, moreThen200Plays, "Songs That received 200 or more plays:");
-           
-            var alernativeCount = (from song in songs where song.Genre == "Alternative" select song).Count();
-            result=AddCountsToResult(result, alernativeCount, "Number of Altnerative songs:");
-
-            var hipHopCount = (from song in songs where song.Genre == "Hip-Hop/Rap" select song).Count();
-            result=AddCountsToResult(result, hipHopCount, "Number of Hip-Hop/Rap songs:");
-
-            var selectAlbumFishbowl = from song in songs where song.Album == "Welcome to the Fishbowl" select song;
-            result = AddSongsToResult(result, selectAlbumFishbowl, "Songs from the album Welcome to the Fishbowl:");
-
-            var songsBefore1970 = from song in songs where song.Year < 1970 select song;
-            result = AddSongsToResult(result, songsBefore1970, "Songs from before 1970:");
-
-            var songNamesLongerThen85Char = from song in songs where song.Name.Length > 85 select song;
-            result = AddSongsToResult(result, songNamesLongerThen85Char, "Song names longer than 85 characters:");
-
-            var longestSong = (from song in songs orderby song.Time descending select song).First();
-            result += "longest Song:\n";
-            result += longestSong;
-
-            return result;
-        }
-
-        static String AddSongsToResult(String result,System.Collections.Generic.IEnumerable<Song> songs,String title)
-        {
-            result += title + "\n";
-            foreach(Song song in songs)
+            List<Song> songs = null;
+            try
             {
-                result += song+"\n";
+                songs = ReadSongs(infile);
             }
-            result += "\n\n";
-            return result;
-           
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Playlist data file can't be opened.");
+                return;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error occurs reading lines from playlist data file.");
+                return;
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+
+            try {
+                FileStream fout = new FileStream(outfile, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fout);
+                Q1(songs, sw);
+                Q2(songs, sw);
+                Q3(songs, sw);
+                Q4(songs, sw);
+                Q5(songs, sw);
+                Q6(songs, sw);
+                Q7(songs, sw);
+                sw.Flush();
+                fout.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Report file can t be opened or written to.");
+            }
         }
-        static String AddCountsToResult(String result,int count, String title)
+
+        static void Q1(List<Song> songs, StreamWriter sw)
         {
-            result += title + "\n";
-            result += count;
-            result += "\n\n";
-            return result;
+            var result = from song in songs where song.Plays >= 200 select song;
+            sw.WriteLine("Songs that received 200 or more plays:");
+            foreach (var song in result)
+            {
+                sw.WriteLine(song.ToString());
+            }
+            sw.WriteLine();
+        }
+
+        static void Q2(List<Song> songs, StreamWriter sw)
+        {
+            var result = from song in songs where song.Genre == "Alternative" select song;
+            sw.WriteLine(string.Format("Number of Alterative songs: {0}", result.Count()));
+            sw.WriteLine();
+
+        }
+
+        static void Q3(List<Song> songs, StreamWriter sw)
+        {
+            var result = from song in songs where song.Genre.Trim().ToLower() == "Hip-Hop/Rap".ToLower() select song;
+            sw.WriteLine(string.Format("Number of Hip-Hop/Rap songs: {0}", result.Count()));
+            sw.WriteLine();
+        }
+
+        static void Q4(List<Song> songs, StreamWriter sw)
+        {
+            var result = from song in songs where song.Album == "Welcome to the Fishbowl" select song;
+            sw.WriteLine("Songs from the album Welcome to the fishbowl:");
+            foreach (var song in result)
+            {
+                sw.WriteLine(song.ToString());
+            }
+            sw.WriteLine();
+        }
+
+        static void Q5(List<Song> songs, StreamWriter sw)
+        {
+            sw.WriteLine("Songs from before 1970: ");
+            var result = from song in songs where song.Year<1970 select song;
+            foreach (var song in result)
+            {
+                sw.WriteLine(song.ToString());
+            }
+            sw.WriteLine();
+        }
+
+        static void Q6(List<Song> songs, StreamWriter sw)
+        {
+            sw.WriteLine("Song names longer than 85 characters: ");
+            var result = from song in songs where song.Name.Length > 85 select song;
+            foreach (var song in result)
+            {
+                sw.WriteLine(song.ToString());
+            }
+            sw.WriteLine();
+        }
+
+        static void Q7(List<Song> songs, StreamWriter sw)
+        {
+            var r = from song in songs orderby song.Time descending select song;
+            var longestSong = r.First();
+            sw.WriteLine("Longest song: "+longestSong);
+            sw.WriteLine();
         }
 
     }
--- MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Song.cs
+++ MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Song.cs
@@ -1,21 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace MusicPlaylistAnalyzer
 {
     public class Song
     {
-        public String Name { get; set; }
-        public String Artist { get; set; }
-        public String Album { get; set; }
-        public String Genre { get; set; }
-        public long Size { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public int Size { get; set; }
         public int Time { get; set; }
         public int Year { get; set; }
         public int Plays { get; set; }
 
-        override public string ToString()
+        public override string ToString()
         {
-            return String.Format("Name: {0}, Artist: {1}, Album: {2}, Genre: {3}, Size: {4}, Time: {5}, Year: {6}, Plays: {7}", Name, Artist, Album, Genre, Size, Time, Year, Plays);
+            return string.Format("Name: {0}, Artist: {1}, Album: {2}, Genre: {3}, Size: {4}, Time: {5}, Year: {6}, Plays: {7}",
+                Name, Artist, Album, Genre, Size, Time, Year, Plays);
         }
-
     }
 }
